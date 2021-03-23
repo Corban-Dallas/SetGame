5c2404071417d8c15d4689550d0eaedb839fa59f
Grigory Krivyakin <grisha992@gmail.com>
2021-03-23 22:39:18 +0700

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Григорий Кривякин on 23.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
